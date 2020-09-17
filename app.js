1adb9ccddf25cfc70285248a9d61cabebc16f266
Lee Macintyre <lazyislee@googlemail.com>
2020-09-17 06:43:29 +0100

    todo practice

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = [];
+let workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("lists", { listTitle: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("lists", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("your server is running on port 3000");
+});
